d708892ca6628d352e7c0c0518d193cf4db07820
Bishal Gharti Chhetri <chhetrig.bishal@gmail.com>
2025-08-20 22:22:32 +0545

    Remove Eventing.Web project and related references
    Deleted the Eventing.Web project, its source files, configuration, and dependencies. Also removed the reference to Eventing.Web from Eventing.AppHost.csproj and updated the solution file accordingly.

--- Eventing.Web/Components/Features/Login/Dtos/LoginResponseDto.cs
+++
@@ -1,6 +0,0 @@
-using Eventing.Web.Shared.Dtos;
-
-namespace Eventing.Web.Components.Features.Login.Dtos;
-
-public sealed record LoginResponseDto(string AccessToken, long ExpiresIn) 
-    : AuthCredentials(AccessToken, ExpiresIn);
--- Eventing.Web/Shared/Dtos/AuthCredentials.cs
+++
@@ -1,5 +0,0 @@
-namespace Eventing.Web.Shared.Dtos;
-
-public record AuthCredentials(
-    string AccessToken,
-    long ExpiresIn);
